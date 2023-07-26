c546b5517016f2acddd797d8138e3a90d2e5fc0d
Kwon <gunhun963@gmail.com>
2023-07-26 16:56:00 +0900

    first commit
    0726

---
+++ fastApiProject/frontend/src/routes/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+    import fastapi from "../lib/api.js";
+    import { link } from 'svelte-spa-router'
+
+    let question_list = []
+  
+    function get_question_list() {
+      fastapi('get', '/api/question/list', {}, (json) => {
+        question_list = json
+      })
+    }
+  
+    get_question_list()
+  </script>
+  
+  <ul>
+    {#each question_list as question}
+      <li><a use:link href="/detail/{question.id}">{question.subject}</a></li>
+    {/each}
+  </ul>
